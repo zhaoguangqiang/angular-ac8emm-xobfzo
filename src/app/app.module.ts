1fbb12c3875c40bd233dd0d691f45dbc35053f9d
zhaoguangqiang <zhaoguangqiang12@gmail.com>
2020-02-11 16:25:03 +0800

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { ProductAlertsComponent } from
 import { ProductDetailsComponent } from
 './product-details/product-details.component';
 
+import { CartComponent }  from
+'./cart/cart.component'
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -21,6 +24,7 @@ import { ProductDetailsComponent } from
       { path: '', component: ProductListComponent },
       { path: '', component: ProductAlertsComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
