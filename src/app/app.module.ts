1346436f9f949ebb87e5d754afae094af75f28f4
zhaoguangqiang <zhaoguangqiang12@gmail.com>
2020-02-11 16:27:18 +0800

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,9 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 import { ProductAlertsComponent } from
 './product-alerts/product-alerts.component';
-
 import { ProductDetailsComponent } from
 './product-details/product-details.component';
-
-import { CartComponent }  from
+import { CartComponent } from
 './cart/cart.component'
 
 @NgModule({
